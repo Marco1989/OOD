f6fe4a68460d5ad5fe0d247841ae7e45f4cdded1
mahanchao <mahanchao@gmail.com>
2014-08-08 16:43:29 -0400

    second

---
+++ OOD/src/StarBuzz/Espresso.java
@@ -0,0 +1,18 @@
+package StarBuzz;
+
+
+public class Espresso extends Beverage {
+         public Espresso() {
+      
+         description = "Espresso";
+
+         }
+
+         public double cost() {
+
+        	 return 1.99;
+
+         }
+
+
+}
